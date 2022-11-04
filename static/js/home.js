ff552a1c01194c300e5b26c63b022a7f6305f15c
enfyshsu <wch8831@gmail.com>
2022-11-05 00:38:05 +0800

    initial commit

---
+++ static/js/home.js
@@ -0,0 +1,29 @@
+anime.timeline({loop: false})
+  .add({
+    targets: '.shift-in-1',
+    translateX: [40,0],
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: (el, i) => 800 + 50 * i
+  });
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.shift-in-2',
+    translateX: [40,0],
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: (el, i) => 1000 + 50 * i
+  });
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.shift-in-3',
+    translateX: [40,0],
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: (el, i) => 1200 + 50 * i
+  });
