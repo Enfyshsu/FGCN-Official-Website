c06d690eca75f6017c9eb4a5514229e91b6f81de
enfyshsu <wch8831@gmail.com>
2022-11-05 17:58:06 +0800

    research almost done

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $('#navbar').load('view/nav.html');
+  $('#home').load('view/home.html');
   $('#research').load('view/research.html');
+  $('#activity').load('view/activity.html');
 });
 
 anime.timeline({loop: false})
